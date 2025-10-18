860784c4613493a148644f8d5133701ab1a5706b
Saurabh Kumar <Saurabhtbj143@gmail.com>
2025-10-18 18:24:06 +0530

    final updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,24 +14,24 @@ export default defineConfig({
       }
     },
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
+    // Use esbuild for faster builds (alternative to Terser)
+    minify: 'esbuild',
+    // Or if you want to use Terser specifically:
+    // minify: 'terser',
+    // terserOptions: {
+    //   compress: {
+    //     drop_console: true,
+    //     drop_debugger: true
+    //   }
+    // }
   },
   server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    },
     headers: {
       'Cache-Control': 'no-cache'
     }
+  },
+  // Ensure dependencies are pre-bundled
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom', 'axios']
   }
 });
